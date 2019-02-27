d2b5b1adad39838d169af4ffa139dffe7597a4ad
Your Name <you@example.com>
2019-02-26 21:38:54 -0500

    pushing

---
+++ controller.py
@@ -0,0 +1,130 @@
+#!usr/bin/env python3
+import requests
+import random
+from datetime import datetime
+from flask_humanize import Humanize
+import model as m
+
+from flask import Flask, render_template, request, redirect, session
+
+app = Flask(__name__)
+app.secret_key = 'paulina is cool'
+
+HUMANIZE_USE_UTC = True
+
+@app.route('/mainmenu',methods=['GET'])
+def website():
+    if request.method=="GET":
+        return render_template('mainmenu.html')
+    else:
+        return render_template('mainmenu.html')
+
+@app.route('/weather', methods=['GET','POST'])
+def weather():
+    if request.method=="GET":
+        return render_template('weather.html')
+    elif request.method=="POST":
+        submitted_city = request.form['city']
+        check_valid_city = m.check_status_code(submitted_city)
+        
+        if check_valid_city == True:
+            session['submitted_city'] = submitted_city
+            return redirect('/outfits')
+        else:
+            return redirect('/weather')
+
+
+@app.route('/outfits', methods=['GET','POST'])
+def outfits():
+    if request.method=="GET":
+    
+        submitted_city = session.get('submitted_city', None)
+        result = m.getWeather(submitted_city)
+        random.seed(datetime.now())
+        random_num = random.randint(0, 4)
+        
+        cold_footwear = ["Boots", "Hiking Boots", "Uggs", "Winter Moccasins", "Leather Shoes"]
+        cold_top = ["Jacket", "Parka", "Overcoat", "Jacket Shell", "Fur Coat"]
+        cold_accessories = ["Hat", "Toque", "Mittens", "Gloves", "Scarf", "Thermal Base Layers"]
+        cold_bottoms = ["Jeans", "Sweatpants", "Leggings", "Jeggings", "Khakis"]
+
+        mild_footwear = ["Running Shoes", "Dress Shoes", "Slip-On Casual Shoes", "Slides", "Heels"]
+        mild_top = ["T-Shirt", "Long-Sleeve Shirt", "Light Sweatshirt", "Jean Jacket", "Dress Shirt"]
+        mild_accessories = ["Baseball Cap", "Headband", "Parasol", "Bucket Hat", "Watch"]
+        mild_bottoms = ["Sweatpants", "Long Skirt", "Jeans", "Cargo Pants", "Dress Pants", "Leggings"]
+
+        hot_footwear = ["Flip-Flops", "Sandals", "Slides", "Running Shoes", "Slip-On Casual Shoes"]
+        hot_top = ["Tank Top", "T-Shirt", "Undershirt", "Polo", "Blouse"]
+        hot_accessories = ["Fan", "Water Bottle", "Sunscreen", "Parasol", "Sunglasses"]
+        hot_bottoms = ["Short Skirt", "Cargo Shorts", "Jean Shorts", "Trackpants", "Athletic Shorts"]
+
+        jackets = ["Jacket", "Parka", "Overcoat", "Jacket Shell", "Jean Jacket"]
+        t_shirt = ["T-Shirt", "Tank Top", "Undershirt", "Polo"]
+        long_sleeve_shirt = ["Long-Sleeve Shirt", "Light Sweatshirt", "Dress Shirt", "Blouse"]
+
+        boots = ["Boots", "Hiking Boots", "Uggs", "Leather Shoes"]
+        joggers = ["Running Shoes", "Slip-On Casual Shoes"]
+        sandals = ["Slides", "Flip-Flops", "Sandals"]
+        miscellanouse_shoes = ["Winter Moccasins", "Dress Shoes"]
+
+        full_length = ["Jeans", "Sweatpants", "Khakis", "Long Skirt", "Cargo Pants", "Dress Pants"]
+        half_length = ["Short Skirt", "Cargo Shorts", "Jean Shorts", "Athletic Shorts"]
+
+        weather = ["Thunderstorm", "Drizzle", "Rain", "Snow", "Atmosphere", "Clear", "Clouds"]
+
+
+        return render_template('outfits.html', city = submitted_city, result = result, randomInt = random_num, coldFootwear = cold_footwear,
+        coldBottoms = cold_bottoms, coldTop = cold_top, coldAccessories = cold_accessories,
+        mildFootwear = mild_footwear, mildTop = mild_top, mildAccessories = mild_accessories, mildBottoms = mild_bottoms,
+        hotFootwear = hot_footwear, hotTop = hot_top, hotAccessories = hot_accessories, hotBottoms = hot_bottoms, 
+        jacketsIcon = jackets, tShirtIcon = t_shirt, longSleeveIcon = long_sleeve_shirt, bootsIcon = boots,
+        joggersIcon = joggers, sandalsIcon = sandals, miscShoes = miscellanouse_shoes, weather = weather, longPants = full_length,
+        shortPants = half_length)
+    else:
+        submitted_city = session.get('submitted_city', None)
+        result = m.getWeather(submitted_city)
+        random.seed(datetime.now())
+        random_num = random.randint(0, 4)
+        
+        cold_footwear = ["Boots", "Hiking Boots", "Uggs", "Winter Moccasins", "Leather Shoes"]
+        cold_top = ["Jacket", "Parka", "Overcoat", "Jacket Shell", "Fur Coat"]
+        cold_accessories = ["Scarf", "Thermal Base Layers"]
+        cold_bottoms = ["Jeans", "Sweatpants", "Leggings", "Jeggings", "Khakis"]
+
+        mild_footwear = ["Running Shoes", "Dress Shoes", "Slip-On Casual Shoes", "Slides", "Heels"]
+        mild_top = ["T-Shirt", "Long-Sleeve Shirt", "Light Sweatshirt", "Jean Jacket", "Dress Shirt"]
+        mild_accessories = ["Baseball Cap", "Headband", "Bucket Hat", "Watch"]
+        mild_bottoms = ["Sweatpants", "Long Skirt", "Jeans", "Cargo Pants", "Dress Pants", "Leggings"]
+
+        hot_footwear = ["Flip-Flops", "Sandals", "Slides", "Running Shoes", "Slip-On Casual Shoes"]
+        hot_top = ["Tank Top", "T-Shirt", "Undershirt", "Polo", "Blouse"]
+        hot_accessories = ["Fan", "Water Bottle", "Sunscreen", "Sunglasses"]
+        hot_bottoms = ["Short Skirt", "Cargo Shorts", "Jean Shorts", "Trackpants", "Athletic Shorts"]
+
+        jackets = ["Jacket", "Parka", "Overcoat", "Jacket Shell", "Fur Coat", "Jean Jacket"]
+        t_shirt = ["T-Shirt", "Tank Top", "Undershirt", "Polo"]
+        long_sleeve_shirt = ["Long-Sleeve Shirt", "Light Sweatshirt", "Dress Shirt", "Blouse"]
+
+        boots = ["Boots", "Hiking Boots", "Uggs", "Leather Shoes"]
+        joggers = ["Running Shoes", "Slip-On Casual Shoes"]
+        sandals = ["Slides", "Flip-Flops", "Sandals"]
+        miscellanouse_shoes = ["Winter Moccasins", "Dress Shoes", "Heels"]
+
+        full_length = ["Jeans", "Sweatpants", "Leggings", "Jeggings", "Khakis", "Long Skirt", "Cargo Pants", "Dress Pants", "Trackpants"]
+        half_length = ["Short Skirt", "Cargo Shorts", "Jean Shorts", "Athletic Shorts"]
+
+        weather = ["Thunderstorm", "Drizzle", "Rain", "Snow", "Atmosphere", "Clear", "Clouds"]
+
+
+        return render_template('outfits.html', city = submitted_city, result = result, randomInt = random_num, coldFootwear = cold_footwear,
+        coldBottoms = cold_bottoms, coldTop = cold_top, coldAccessories = cold_accessories,
+        mildFootwear = mild_footwear, mildTop = mild_top, mildAccessories = mild_accessories, mildBottoms = mild_bottoms,
+        hotFootwear = hot_footwear, hotTop = hot_top, hotAccessories = hot_accessories, hotBottoms = hot_bottoms, 
+        jacketsIcon = jackets, tShirtIcon = t_shirt, longSleeveIcon = long_sleeve_shirt, bootsIcon = boots,
+        joggersIcon = joggers, sandalsIcon = sandals, miscShoes = miscellanouse_shoes, halfBottomsIcon = half_length,
+        fullBottomsIcon = full_length, weather = weather)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ model.py
@@ -0,0 +1,26 @@
+import requests
+
+#class WeatherInfo:
+def getWeather(city):
+    hourlyWeather = {}
+    weatherData = requests.get('http://api.openweathermap.org/data/2.5/forecast?q={}&APPID=225800a064f12ac6d7fbaadf83ce753f'.format(city))
+    weatherData = weatherData.json()
+    for x in range(8):
+        dateTime = weatherData['list'][x]['dt_txt']
+        timeArr = dateTime.rsplit(" ")
+        date = timeArr[0]
+        time = timeArr[1]
+        if (time == "12:00:00"):
+            hourlyWeather["Temperature"] = round((weatherData['list'][x]['main']['temp'] - 273.15), 1)
+            hourlyWeather["Weather"] = weatherData['list'][x]['weather'][0]['main']
+            hourlyWeather["Weather Details"] = weatherData['list'][x]['weather'][0]['description']
+            hourlyWeather["Wind Speed"] = round((weatherData['list'][x]['wind']['speed']),2)
+            hourlyWeather["Date"] = date
+    return hourlyWeather
+
+def check_status_code(city):
+    response = requests.get('http://api.openweathermap.org/data/2.5/forecast?q={}&APPID=225800a064f12ac6d7fbaadf83ce753f'.format(city))
+    if int(response.status_code) != 200:
+        return False
+    else:
+        return True
